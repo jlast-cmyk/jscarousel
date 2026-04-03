827374d3e22109a5f9e244edddbad841cd84df60
jlast-cmyk <jlast@mail.endicott.edu>
2026-04-03 09:11:19 -0400

    carosel

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,36 @@
 window.onload = function () {
     document.getElementById("copyright").innerHTML = new Date().getFullYear();
 }
+
+
+
+//SLideshow function
+let intIndex = 0;
+
+function showImage(i) {
+    intIndex+=i;
+
+    let theImages = document.getElementsByClassName("image");
+    let theDots = document.getElementsByClassName("dot");
+
+    for (n=0; n<theImages.length; n++) {
+        theImages[n].style.display = "None"
+        theDots[n].className = theDots[n].className.replace(" active", "")
+    }
+
+    if (intIndex>theImages.length-1){
+        intIndex = 0;
+    }
+    if(intIndex < 0){
+        intIndex = theImages.length -1;
+    }
+
+    theImages[intIndex].style.display = "block";
+    theDots[intIndex].className += " active";
+}
+
+setInterval(() => {
+    showImage(1)
+}, 5000);
+
+
